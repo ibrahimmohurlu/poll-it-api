deff24e9b5530001a97d84dda39843383da6afb7
ibrahimmohurlu <ibrahimmohurlu@gmail.com>
2023-04-10 00:59:29 +0300

    Prisma module implemented.

--- src/poll/poll.module.ts
+++ src/poll/poll.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
 import { PollService } from "./poll.service";
 import { PollController } from "./poll.controller";
+import { PrismaModule } from "src/prisma/prisma.module";
 
 @Module({
   providers: [PollService],
   controllers: [PollController],
+  imports: [PrismaModule],
 })
 export class PollModule {}
--- src/poll/poll.service.ts
+++ src/poll/poll.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
 export class PollService {
-  getAllPolls() {
-    return [{ title: "poll1" }, { title: "poll2" }];
+  constructor(private prismaService: PrismaService) {}
+  async getAllPolls() {
+    return await this.prismaService.poll.findMany();
   }
 }
