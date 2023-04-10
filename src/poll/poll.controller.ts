9656fc6101340cbe136d1d216c763a055301373d
ibrahimmohurlu <ibrahimmohurlu@gmail.com>
2023-04-10 03:44:07 +0300

    poll enpoints and Validation pipes implemented.

--- src/poll/poll.controller.ts
+++ src/poll/poll.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post } from "@nestjs/common";
 import { PollService } from "./poll.service";
+import { CreatePollDto } from "./dto/create-poll.dto";
 
-@Controller("api/polls")
+@Controller("polls")
 export class PollController {
   constructor(private pollService: PollService) {}
 
   @Get("/")
   getAllPolls() {
     return this.pollService.getAllPolls();
+  }
+
+  @Get("/:id")
+  getPollById(@Param("id") pollId) {
+    return this.pollService.getPollById(pollId);
+  }
+
+  @Post("/")
+  createPoll(@Body() createPollDto: CreatePollDto) {
+    return this.pollService.createPoll(createPollDto);
+  }
+
+  @Delete("/:id")
+  deletePollById(@Param("id") pollId) {
+    return this.pollService.deletePollById(pollId);
   }
 }
--- src/poll/poll.service.ts
+++ src/poll/poll.service.ts
@@ -1,10 +1,66 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
+import { CreatePollDto } from "./dto/create-poll.dto";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 @Injectable()
 export class PollService {
   constructor(private prismaService: PrismaService) {}
   async getAllPolls() {
-    return await this.prismaService.poll.findMany();
+    return await this.prismaService.poll.findMany({
+      include: { poll_options: true },
+    });
+  }
+
+  async getPollById(pollId: string) {
+    return await this.prismaService.poll.findUnique({
+      where: { id: pollId },
+      include: {
+        poll_options: true,
+      },
+    });
+  }
+
+  async createPoll(pollData: CreatePollDto) {
+    return await this.prismaService.poll.create({
+      data: {
+        question: pollData.question,
+        poll_options: {
+          create: pollData.options.map((option) => {
+            return {
+              body: option.body,
+            };
+          }),
+        },
+      },
+      include: {
+        poll_options: true,
+      },
+    });
+  }
+
+  async deletePollById(pollId: string) {
+    try {
+      const deletedPoll = await this.prismaService.poll.delete({
+        where: {
+          id: pollId,
+        },
+        include: {
+          poll_options: true,
+        },
+      });
+      return deletedPoll;
+    } catch (e: any) {
+      if (e instanceof PrismaClientKnownRequestError) {
+        throw new HttpException(
+          `Poll with id ${pollId} not found`,
+          HttpStatus.NOT_FOUND,
+        );
+      }
+      throw new HttpException(
+        "Internal Server Error",
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
