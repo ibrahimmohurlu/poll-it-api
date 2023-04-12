8b173f976e097067b245e1a9d232cdd6aa541b45
ibrahimmohurlu <ibrahimmohurlu@gmail.com>
2023-04-13 02:02:16 +0300

    swagger added to the project.

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { HttpAdapterHost, NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { ValidationPipe } from "@nestjs/common";
 import { PrismaClientExceptionFilter } from "./prisma-client-exception/prisma-client-exception.filter";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,16 @@ async function bootstrap() {
 
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+
+  const config = new DocumentBuilder()
+    .setTitle("Poll It API")
+    .setDescription("poll-it-api description")
+    .setVersion("1.0")
+    .addTag("polls")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+
   await app.listen(3000);
 }
 bootstrap();
