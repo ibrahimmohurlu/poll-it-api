0a173d8f39c610e1770a8d6a9949856c50518f2a
ibrahimmohurlu <44712700+ibrahimmohurlu@users.noreply.github.com>
2024-02-14 02:18:06 +0300

    swagger docs path updated.

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
     .setVersion("1.0")
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("/", app, document);
+  SwaggerModule.setup("/api/docs", app, document);
 
   await app.listen(3000);
 }
